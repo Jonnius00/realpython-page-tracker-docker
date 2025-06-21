d7651720a5070e5aafe272c588ae0a89650c9e19
JonniusDD <itfsoft@gmail.com>
2025-06-21 23:55:01 +0200

    refactor: update Redis integration and error handling in index function

--- src/page_tracker/app.py
+++ src/page_tracker/app.py
@@ -1,10 +1,34 @@
+import os
+from functools import cache
 from flask import Flask
-from redis import Redis
+from redis import Redis, RedisError
 
 app = Flask(__name__)
-redis = Redis()
 
+
+# controller function
 @app.get("/")
 def index():
-    page_views = redis.incr("page_views")
-    return f"This page has been visited {page_views} times... "
+    try:
+        page_views = redis().incr("page_views")
+    except RedisError:  
+        app.logger.exception("Redis error")     # pylint: disable=E1101
+        # tells pylint to ignore a particular instance of the error E1101 without suppressing it completely
+        return "Sorry, something went wrong \N{pensive face}", 500
+    else:
+        return f"This page has been seen {page_views} times."
+
+
+@cache
+def redis():
+    # return Redis()
+    # return Redis(host="127.0.0.1", port=6379)
+    # return Redis.from_url( "redis://localhost:6379/" )
+    return Redis.from_url( os.getenv("REDIS_URL", "redis://localhost:6379/") )
+    
+    # return Redis(host="redis-server", port=6379, db=0, decode_responses=True)
+
+# This name-main idiom pattern added to let bandit or a similar tool 
+# test the App against vulnerabilities before deploying the code to production.
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
