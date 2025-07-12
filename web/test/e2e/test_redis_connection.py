d8988f8b85d5e8700ee9edd6b12ccac72a5ec7a2
JonniusDD <itfsoft@gmail.com>
2025-07-12 22:50:39 +0200

    refactor redis connection test to use common fixture from conftest.py

--- web/test/e2e/test_redis_connection.py
+++ web/test/e2e/test_redis_connection.py
@@ -1,11 +1,19 @@
 import pytest
 import redis
+import os
 
-@pytest.fixture
+# defining a separate own fixture for this test, but
+# should use the redis_client fixture from conftest.py
+""" @pytest.fixture
 def redis_connection():
-    client = redis.StrictRedis(host='localhost', port=6379, db=0)
+    redis_url = os.environ.get("REDIS_URL", "redis://redis-service:6379")
+    client = redis.from_url(redis_url)
     yield client
     client.close()
 
 def test_redis_connection(redis_connection):
     assert redis_connection.ping() == True
+"""
+
+def test_redis_connection(redis_client):
+    assert redis_client.ping() == True
